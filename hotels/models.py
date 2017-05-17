e6128be8e9cca2bf8a5b8104a2d134779878aa9c
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-17 20:10:15 +0200

    add description field

--- hotels/models.py
+++ hotels/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
 class Hotel(models.Model):
 	name = models.CharField(max_length=50)
 	slug = models.SlugField(max_length=40)
 	price = models.DecimalField(max_digits=8, decimal_places=2)
+	description = models.TextField(null=True, blank=True)
