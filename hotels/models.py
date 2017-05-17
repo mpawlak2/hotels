807ae1e375f6d51f9549f4957917208623d032de
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-17 20:18:13 +0200

    minor updates to model

--- hotels/models.py
+++ hotels/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 class HotelModel(models.Model):
-	name = models.CharField(max_length=50)
+	name = models.CharField(max_length=50, verbose_name='Hotel name:')
 	slug = models.SlugField(max_length=40)
 	price = models.DecimalField(max_digits=8, decimal_places=2)
 	description = models.TextField(null=True, blank=True)
+
+
+
+	class Meta:
+		verbose_name = 'Hotel'
+		verbose_name_plural = 'Hotels'
+
+	def __str__(self):
+		return self.name
