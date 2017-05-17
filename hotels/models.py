78537310877cc7d9b942d6ad8944d73f3017eb8f
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-17 20:01:09 +0200

    create hotel model

--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Hotel(models.Model):
+	name = models.CharField(max_length=50)
+	slug = models.SlugField(max_length=40)
+	price = models.DecimalField(max_digits=8, decimal_places=2)
