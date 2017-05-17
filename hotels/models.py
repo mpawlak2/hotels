da76f6bf60feec06ce2f7f57c71c366f9f17810b
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-17 20:12:57 +0200

    minor update & register in admin

--- hotels/models.py
+++ hotels/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Hotel(models.Model):
+class HotelModel(models.Model):
 	name = models.CharField(max_length=50)
 	slug = models.SlugField(max_length=40)
 	price = models.DecimalField(max_digits=8, decimal_places=2)
