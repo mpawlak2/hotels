b6271eabe157b685a21551cdb2d6adb7f7c19ddb
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-18 16:34:00 +0200

    use signals to crate slug

--- hotels/models.py
+++ hotels/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-
+from django.utils.text import slugify
+from django.db.models.signals import post_save, pre_save
 HOTEL_STARS = [
 		('1', 'One star hotel.'),
 		('2', 'Two stars hotel.'),
@@ -17,12 +18,16 @@ class HotelModel(models.Model):
 	stars 			= models.CharField(max_length=1, verbose_name='Hotel stars', choices=HOTEL_STARS, default='1')
 	insert_date		= models.DateTimeField(auto_now_add=True)
 
-	def save(self, *args, **kwargs):
-		super(HotelModel, self).save(*args, **kwargs)
-
 	class Meta:
 		verbose_name = 'Hotel'
 		verbose_name_plural = 'Hotels'
 
 	def __str__(self):
 		return self.name
+
+
+def hotel_model_pre_save(sender, instance, *args, **kwargs):
+	if not instance.slug and instance.name:
+		instance.slug = slugify(instance.name)
+
+pre_save.connect(hotel_model_pre_save, sender=HotelModel)
