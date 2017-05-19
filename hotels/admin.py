64e9841e39adccad25185e0d54f74761bf7469b0
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-19 21:17:32 +0200

    display active field

--- hotels/admin.py
+++ hotels/admin.py
@@ -14,6 +14,7 @@ class HotelModelAdmin(admin.ModelAdmin):
 			'insert_date',
 			'timestamp',
 			'age',
+			'active',
 	]
 
 	readonly_fields = [
@@ -21,7 +22,7 @@ class HotelModelAdmin(admin.ModelAdmin):
 		'insert_date',
 		'updated',
 		'timestamp',
-		'age',
+		'age', 
 	]
 
 	def age(self, obj, *args, **kwargs):
