c46336d2a82ba777555a5d88a21fdaa0743ecd91
Pawlak Mateusz <mpawlak62@gmail.com>
2017-05-22 23:06:33 +0200

    render form

--- hotels/urls.py
+++ hotels/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
+from .views import show_form
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='hotels/index.html')),
+    url(r'^form/$', show_form),
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import SearchForm
 
 # Create your views here.
+
+def show_form(request):
+	form = SearchForm()
+	return render(request, 'hotels/test_form.html', {'form': form})
